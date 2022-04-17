bcff878e6e4eacb0666d803bcd69a2b93c741769
c3katsu3c <thangyb2706@gmail.com>
2022-04-17 17:32:06 +0700

    thắng 5:31 14/4

---
+++ quanlykho/src/QLK/model/Kho.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package QLK.model;
+
+/**
+ *
+ * @author Pham Thang <Thangyb2706@gmail.com>
+ */
+public class Kho {
+    
+}
